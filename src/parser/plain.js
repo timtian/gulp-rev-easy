c7d24913b47bad2869556a2c7bd11e9eed9c1dc4
timtian <tim@vip.qq.com>
2016-09-09 09:54:25 +0800

    fix some bug

--- src/parser/plain.js
+++ src/parser/plain.js
@@ -7,63 +7,62 @@ var revver = require('../revver');
 
 var parser = {};
 
-parser.parse = function(text, filepath, options) {
+parser.parse = function (text, filepath, options) {
     try {
 
-        var resText= [];
+        var resText = [];
         var textList = [];
         var mList = [];
-        if(options.ignorePattern){
+        if (options.ignorePattern) {
             var tmptextList = text.split(options.ignorePattern);
 
             var m;
             var mindex = 0;
-            textList.push(tmptextList[mindex]);
+            textList.push({raw: tmptextList[mindex], ignore: false});
             while ((m = options.ignorePattern.exec(text)) !== null) {
-                if(options.ignoreFilter(m)){
-                    mList.push(m[0]);
-                }else{
-                    textList.push(m[0]);
-                }
+                textList.push({raw: m[0], ignore: options.ignoreFilter(m)});
                 mindex++;
-                textList.push(tmptextList[mindex]);
+                textList.push({raw: tmptextList[mindex], ignore: false});
             }
-        }else{
-            textList.push(text);
-        };
-
-
-
-        for(var j = 0; j < textList.length; j++){
-            var revText = textList[j];
-
-            for (var i = 0; i < options.fileTypes.length; i++) {
-                var fileType = options.fileTypes[i];
-                var pattern = options.patterns[fileType];
-                revText = revText.replace(pattern.regex, function(match, p1, p2, p3, offset, string){
-
-                    var q = p2.substring(0, 1);
-                    var src = p2.substring(1, p2.length - 1);
-                    var revv = revver.rev(src, filepath, options);
-                    var newsrc = p2;
-
-                    if (revv !== '') {
-                        newsrc = options.transformPath(src, revv);
-                        gutil.log(src + ' --> ', gutil.colors.green(newsrc));
-                    } else {
-                        gutil.log(gutil.colors.blue('ignore:rev is empty'), src);
-                    }
-
-                    return p1 + q +  newsrc + q + p3;
-                })
-            }
-
-            resText.push(revText);
-
-            if(mList.length > 0)
-                resText.push(mList.shift());
+        } else {
+            textList.push({raw:text, ignore:false});
         }
 
+
+
+        for (var j = 0; j < textList.length; j++) {
+            var revObj = textList[j];
+            if (!revObj.ignore) {
+                var revText = revObj.raw;
+                for (var i = 0; i < options.fileTypes.length; i++) {
+                    var fileType = options.fileTypes[i];
+                    var pattern = options.patterns[fileType];
+                    revText = revText.replace(pattern.regex, function (match, p1, p2, p3, offset, string) {
+
+                        var q = p2.substring(0, 1);
+                        var src = p2.substring(1, p2.length - 1);
+                        var revv = revver.rev(src, filepath, options);
+                        var newsrc = p2;
+
+                        if (revv !== '') {
+                            newsrc = options.transformPath(src, revv);
+                            gutil.log(src + ' --> ', gutil.colors.green(newsrc));
+                        } else {
+                            gutil.log(gutil.colors.blue('ignore:rev is empty'), src);
+                        }
+
+                        return p1 + q + newsrc + q + p3;
+                    })
+                }
+                resText.push(revText);
+            }else{
+                resText.push(revObj.raw);
+            }
+
+
+            if (mList.length > 0)
+                resText.push(mList.shift());
+        }
 
 
         return resText.join('');
